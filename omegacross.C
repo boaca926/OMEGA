4b0c2054b250e7284405a031c9ae55fb56472ab6
boaca926 <bo.cao@physics.uu.se>
2017-05-17 17:06:06 +0200

    update

---
+++ omegacross.C
@@ -0,0 +1,56 @@
+#include "Getname.C"
+#include <iostream>
+#include <string>
+#include "gethisto.C"
+double getW0(double alphapi, double x, double theta0) {
+	double W0_temp = 0.;
+	double costheta = TMath::Cos(theta0);
+	double P1 = 0.;
+	double P2 =0.;
+	P1 = 2-2*x+x*x;
+	P2 = x*x;
+	
+	W0_temp = alphapi*(P1*TMath::Log((1+costheta)/(1-costheta))-P2*costheta)/x;	
+			
+	return W0_temp;
+}
+
+
+double getSigma(double sqrtS, double m, double W0, double sigma0) {
+	double sigma_temp = 0.;
+	double S = TMath::Power(sqrtS,2);
+ 
+ 	sigma_temp=(2*m/S)*W0*(sqrtS-m)*sigma0;
+	return sigma_temp;
+}
+
+
+void omegacross() {
+	// define constants
+	const double m_omega = 782.; // MeV
+	const double sqrtS = 1019.; // center of mass energy
+	const double alpha = 1/137.; // fine structure constant
+	const double pi=TMath::Pi();
+	const double alphapi = alpha/pi;
+	const double theta0 = 23.*pi/180.; // polar angle
+	const double sigma0 = 300; // born cross section of e+e->omega in nano bar, mu = 10^-6, femto = 10^-5, nano =10^-9
+	// define variables
+	double x=0.; // effective fraction
+	double W0=0.; // radiative function
+	double sigma=0.; // visible cross section
+	// get values
+	x = 1-TMath::Power(m_omega/sqrtS,2);
+	W0 = getW0(alphapi,x,theta0);
+	sigma=getSigma(sqrtS,m_omega,W0,sigma0);
+	
+	std::cout<<"omega mass = "<<m_omega<<endl;
+	std::cout<<"center-of-mass energy = "<<sqrtS<<endl;
+	std::cout<<"effective fraction = "<<x<<endl;
+	std::cout<<"polar angle = "<<theta0<<endl;
+	std::cout<<"radiative function W0 = "<<W0<<endl;
+	std::cout<<"born cross section sigma0 = "<<sigma0<<endl;
+	std::cout<<"visible cross section of omega "<<sigma<<" at C.o.M energy = "<<sqrtS<<endl;
+
+
+
+}
